df9f43fda42913584f845d4301734fabd8d254c2
Kacper <kgrzesik17@gmail.com>
2025-09-15 20:02:33 +0200

    navbar

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <div class="header__container">
+            <div class="header__brand">
+                <p>Gym Companion</p>
+            </div>
+
+            <div class="header__links">
+                <ul>
+                    <li><a href="#">Trening</a></li>
+                    <li><a href="./cwiczenia">Ćwiczenia</a></li>
+                    <li><a href="./pomiary">Pomiary</a></li>
+                </ul>
+            </div>
+        </div>
+    </header>
+
+    <div class="main">
+        <div class="intro">
+            <h1>Trening</h1>
+        </div>
+    </div>
+</body>
+</html>
